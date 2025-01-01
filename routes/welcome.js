4cc47bd7f99de149260913c12647ba93ab05c7f7
Eric <ewills.ew@gmail.com>
2024-12-31 19:38:58 -0500

    Fix import path for user model

--- routes/welcome.js
+++ routes/welcome.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Import the User model
+const User = require('../models/user'); // Import the User model
 
 // POST endpoint to save user data
 router.post('/data', async (req, res) => {
